466f64fec221b5d942d668be73a3caf425e047b0
polinabersh <bershpolina@gmail.com>
2025-02-22 19:32:10 +0100

    Finish

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnOpen.forEach(button => {button.addEventListener('click', toggleModal);});
 modalBtnClose.addEventListener('click', toggleModal);
